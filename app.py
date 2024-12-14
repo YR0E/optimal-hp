23831c20908890cc97d67015551e2beabb6e828d
Yelnar <87166091+YR0E@users.noreply.github.com>
2024-12-14 18:00:31 +0500

    footer added/ feedback updated

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from google.oauth2.service_account import Credentials
 
 
 # Define constants
-SECTION_TYPES = ["Concept", "Documentation", "Calculation", "Visualization", "Other"]
-FEEDBACK_TYPES = ["Question", "Suggestion", "Request", "Error", "Bug", "Other"]
+# SECTION_TYPES = ["Concept", "Documentation", "Calculation", "Visualization", "Other"]
+FEEDBACK_TYPES = ["Question", "Suggestion", "Feature Request", "Report a Problem", "Other"]
 TIMEZONE = pytz.timezone('Asia/Almaty')
 
 # Define the scope for Google API
@@ -33,15 +33,15 @@ def validate_and_submit_feedback(sheet, feedback_data):
     # Validate the feedback form inputs and submit data to Google Sheets.
 
     name = feedback_data[1]
-    section = feedback_data[2]
-    if not name or not section:
+    type = feedback_data[3]
+    if not name or not type:
         st.warning("Please fill out all required fields.")
     else:
         
-        with st.spinner("Submitting feedback..."):
+        with st.spinner("Sending feedback..."):
             try:
                 sheet.append_rows([feedback_data])
-                st.success("Feedback successfully submitted!")
+                st.success("Feedback sent successfully!")
             except Exception as e:
                 st.error(f"Error updating Google Sheets: {e}")
 
@@ -59,8 +59,8 @@ pg.run()
 
 # Sidebar: Feedback Form
 with st.sidebar:
-    st.markdown("## Feedback Submission")
-    with st.expander("Please leave your feedback here", expanded=True):
+    st.markdown("## Feedback")
+    with st.expander("Drop a line here", expanded=True):
         with st.form(key="feedback_form", clear_on_submit=True, border=False):
             # Retrieve the cached Google Sheet
             sheet = get_google_sheet()
@@ -68,12 +68,16 @@ with st.sidebar:
             # Collecting feedback inputs
             submitting_date = datetime.now(TIMEZONE).strftime('%Y-%m-%d %H:%M:%S')
             name = st.text_input(label="Name* (*required*)")
-            col1, col2 = st.columns(2)
-            section_type = col1.selectbox("Section*", options=SECTION_TYPES, index=None)
-            feedback_type = col2.selectbox("Feedback Type", options=FEEDBACK_TYPES, index=None)
+            # col1, col2 = st.columns(2)
+            # section_type = col1.selectbox("Section*", options=SECTION_TYPES, index=None)
+            section_type = 'General'
+            feedback_type = st.selectbox("Feedback Type*", options=FEEDBACK_TYPES, index=None)
             comment = st.text_area(label="Comments")
             feedback_data = [submitting_date, name, section_type, feedback_type, comment]
 
-            submit_button = st.form_submit_button(label="Leave feedback")
+            submit_button = st.form_submit_button(label="Send")
             if submit_button:
                 validate_and_submit_feedback(sheet, feedback_data)
+
+    st.markdown("---")
+    st.markdown("by [Yelnar Yerdesh](https://github.com/YR0E/)")
