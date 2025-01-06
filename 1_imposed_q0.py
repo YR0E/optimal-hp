0c2fe3411b4493738e74951c9584ae90ddadb371
Yelnar <yelnaryerdesh@gmail.com>
2025-01-06 20:05:36 +0500

    refactor: 3d plot

--- 1_imposed_q0.py
+++ 1_imposed_q0.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from streamlit_theme import st_theme
 from util.calc_imposed_q0 import find_minimum, objective_function, objective_function_ir_ratio, objective_function_ep_rate
+from util.plot import plotting3D
 from util.navigation import link_to_pages
 
 
@@ -63,7 +64,6 @@ DEFAULT_VALUES_SA_E = {
     's_sae': 1.1
 }
 
-
 def init_session_state(default):
     for key, value in default.items():
         if key not in st.session_state:
@@ -91,7 +91,7 @@ with tab_eps_total:
 
     col_control, _, col_plot = st.columns((0.34, 0.02, 0.64))
 
-    #========SLIDERS========
+    #=====SLIDERS=====
     with col_control:
     # with col_control.form("sliders", border=False):
         init_eps_total = init_slider(r'$\varepsilon_{total}:$', 'eps_total', 
@@ -112,162 +112,37 @@ with tab_eps_total:
         st.button("Reset", on_click=lambda: reset_sliders(DEFAULT_VALUES_EPS))
 
 
-    initial_params = [init_c_g, init_c_p, init_eps_total, init_q, init_t_s, MULTIPLIER]
-    initial_params_ir_ratio = [init_c_g, init_c_p, init_eps_total, init_q, init_t_s, init_I, MULTIPLIER]
-    initial_params_ep_rate = [init_c_g, init_c_p, init_eps_total, init_q, init_t_s, init_s, MULTIPLIER]
-
-
     # Perform optimization
-    with st.spinner("Optimizing..."):
-        opt_var = 'e'
-        res = find_minimum(objective_function, initial_params, opt_var)
-        res_ir_ratio = find_minimum(objective_function_ir_ratio, initial_params_ir_ratio, opt_var)
-        res_ep_rate = find_minimum(objective_function_ep_rate, initial_params_ep_rate, opt_var)
+    initial_params = {
+        'r': [init_c_g, init_c_p, init_eps_total, init_q, init_t_s, MULTIPLIER],
+        'ir': [init_c_g, init_c_p, init_eps_total, init_q, init_t_s, init_I, MULTIPLIER],
+        'ep': [init_c_g, init_c_p, init_eps_total, init_q, init_t_s, init_s, MULTIPLIER]
+    }
+    opt_var = 'e'
+    results = {
+        'r': find_minimum(objective_function, initial_params['r'], opt_var),
+        'ir': find_minimum(objective_function_ir_ratio, initial_params['ir'], opt_var),
+        'ep': find_minimum(objective_function_ep_rate, initial_params['ep'], opt_var)
+    }
+
+    #=====PLOT=====
+    with col_plot:
+        plotting3D(results, initial_params, opt_var)
 
 
+    st.write('#### Results:')
     df = pd.DataFrame({
         'I': [np.NaN, init_I, np.NaN],
         f's [e-{POWER_OF_10:.0f}]': [np.NaN, np.NaN, init_s],
-        'e*_g': [res.x[0], res_ir_ratio.x[0], res_ep_rate.x[0]],
-        'e*_p': [res.x[1], res_ir_ratio.x[1], res_ep_rate.x[1]],
-        'e*_ev': [res.x[2], res_ir_ratio.x[2], res_ep_rate.x[2]],
-        'e*_cd': [res.x[3], res_ir_ratio.x[3], res_ep_rate.x[3]],
-        f'min(w) [e-{POWER_OF_10:.0f}]': np.array([res.fun, res_ir_ratio.fun, res_ep_rate.fun]) * MULTIPLIER
+        'e*_g': [results['r'].x[0], results['ir'].x[0], results['ep'].x[0]],
+        'e*_p': [results['r'].x[1], results['ir'].x[1], results['ep'].x[1]],
+        'e*_ev': [results['r'].x[2], results['ir'].x[2], results['ep'].x[2]],
+        'e*_cd': [results['r'].x[3], results['ir'].x[3], results['ep'].x[3]],
+        f'min(w) [e-{POWER_OF_10:.0f}]': np.array([results['r'].fun, results['ir'].fun, results['ep'].fun]) * MULTIPLIER
         }, 
         index=['reversibility', 'irreversibility ratio', 'entropy production rate']
     )
-
-
-
-    #=======PLOT=======
-    # Generate data for surface plot
-    step_size = 0.01
-    e_g_values = np.arange(0.1, 1.0001, step_size)
-    e_p_values = np.arange(0.1, 1.0001, step_size)
-    X, Y = np.meshgrid(e_g_values, e_p_values)
-    Z = objective_function([X, Y, X, Y], initial_params, opt_var) * MULTIPLIER
-    Z_ir = objective_function_ir_ratio([X, Y, X, Y], initial_params_ir_ratio, opt_var) * MULTIPLIER
-    Z_ep = objective_function_ep_rate([X, Y, X, Y], initial_params_ep_rate, opt_var) * MULTIPLIER
-
-    # contraint line
-    if init_eps_total/2 <= 1.1: 
-        X_line = np.linspace(0.1, init_eps_total/2 - 0.1, 21)
-    else: X_line = np.linspace(init_eps_total/2 - 1, 1, 21)
-
-    min_line_lim = min(np.min(Z), np.min(Z_ir), np.min(Z_ep))
-    max_line_lim = max(np.max(Z), np.max(Z_ir), np.max(Z_ep))
-
-    X_line, Z_line = np.meshgrid(X_line, np.linspace(min_line_lim, max_line_lim, 21))
-    Y_line = init_eps_total/2 - X_line
-
-    # minimum points
-    x_r, y_r, z_r = res.x[0], res.x[1], res.fun * MULTIPLIER
-    x_ir, y_ir, z_ir = res_ir_ratio.x[0], res_ir_ratio.x[1], res_ir_ratio.fun * MULTIPLIER
-    x_ep, y_ep, z_ep = res_ep_rate.x[0], res_ep_rate.x[1], res_ep_rate.fun * MULTIPLIER
-
-
-    # Set the configuration for the Plotly chart, including the resolution settings
-    config = {
-        "toImageButtonOptions": {
-            "format": "png",  # The format of the exported image (png, svg, etc.)
-            "filename": "surface_plot",  # Default filename
-            # "height": 1080,  # Image height
-            # "width": 1920,   # Image width
-            "scale": 3       # Increase the resolution (scales up the image)
-        }
-    }
-    contours = dict(
-        x=dict(
-            show=True,
-            usecolormap=True,
-            highlight=True,
-            highlightcolor="white",
-        ),
-        y=dict(
-            show=True,
-            usecolormap=True,
-            highlight=True,
-            highlightcolor="white",
-        )
-    )
-
-    fig = go.Figure()
-
-    fig.add_trace(go.Surface(
-        z=Z, x=X, y=Y, 
-        name='reversibility', legendgroup='reversibility', 
-        colorscale='Viridis', showlegend=True, showscale=False, opacity=0.75,
-        contours=contours
-    ))
-    fig.add_trace(go.Surface(
-        z=Z_ir, x=X, y=Y, 
-        name='irreversibility ratio', legendgroup='irreversibility',
-        colorscale='RdBu_r', showlegend=True, showscale=False, opacity=0.75,
-        contours=contours
-    ))
-    fig.add_trace(go.Surface(
-        z=Z_ep, x=X, y=Y, 
-        name='entropy production rate', legendgroup='entropy production', 
-        colorscale='rdylgn_r', showlegend=True, showscale=False, opacity=0.75,
-        contours=contours
-    ))
-    fig.add_trace(go.Surface(
-        z=Z_line, x=X_line, y=Y_line, 
-        name='constraint', legendgroup='constraint', 
-        colorscale=[[0, 'red'], [1, 'red']], showlegend=True, showscale=False, opacity=0.1,
-    ))
-    fig.add_trace(go.Scatter3d(
-        x=[x_r], y=[y_r], z=[z_r],
-        mode='markers',
-        name='minimum', legendgroup='minimum', showlegend=True,
-        marker=dict(size=5, color='red', symbol='circle'),
-    ))
-    fig.add_trace(go.Scatter3d(
-        x=[x_ir], y=[y_ir], z=[z_ir],
-        mode='markers',
-        name='minimum', legendgroup='minimum', showlegend=False,
-        marker=dict(size=5, color='red', symbol='circle'),
-    ))
-    fig.add_trace(go.Scatter3d(
-        x=[x_ep], y=[y_ep], z=[z_ep],
-        mode='markers',
-        name='minimum', legendgroup='minimum', showlegend=False,
-        marker=dict(size=5, color='red', symbol='circle'),
-    ))
-
-    fig.update_layout(
-        title=dict(text='3D Plot'), 
-        autosize=False,
-        width=600, height=500,
-        margin=dict(l=10, r=10, b=10, t=40),
-        scene=dict(
-            xaxis_title='<i>ε<sub>g</sub></i>',
-            yaxis_title='<i>ε<sub>p</sub></i>',
-            zaxis_title=f'<i>w · 10<sup>−{POWER_OF_10:.0f}</sup></i>',
-            xaxis_title_font=dict(family='STIX Two Math'),
-            yaxis_title_font=dict(family='STIX Two Math'),
-            zaxis_title_font=dict(family='STIX Two Math'),
-            aspectratio=dict(x=1, y=1, z=1),
-            camera=dict(
-                eye=dict(x=2, y=1, z=0.5)
-            )
-        ),
-        legend=dict(
-            yanchor="top",
-            y=1,
-            xanchor="center",
-            x=0.5,
-            orientation="h"
-        )
-    )
-
-
-    with col_plot:
-        st.plotly_chart(fig, use_container_width=True, config=config)
-
-    st.write('#### Results:')
     st.dataframe(df)
-
 
 
 
@@ -278,7 +153,7 @@ with tab_c_total:
 
     col_control, _, col_plot = st.columns((0.34, 0.02, 0.64))
 
-    #========SLIDERS========
+    #=====SLIDERS=====
     with col_control:
         init_c_total = init_slider('$c_{total}:$', 'c_total', 0.1, 1.0, 0.01)
         init_e_g = init_slider(r'$\varepsilon_{g}:$', 'e_g', 0.1, 1.0, 0.01)
@@ -293,149 +168,35 @@ with tab_c_total:
         st.button("Reset", on_click=lambda: reset_sliders(DEFAULT_VALUES_C), key='btn_c')
 
 
-    initial_params_c = [init_e_g, init_e_p, init_c_total, init_q, init_t_s, MULTIPLIER]
-    initial_params_ir_c = [init_e_g, init_e_p, init_c_total, init_q, init_t_s, init_I, MULTIPLIER]
-    initial_params_ep_c = [init_e_g, init_e_p, init_c_total, init_q, init_t_s, init_s, MULTIPLIER]
-
-
     # Perform optimization
-    with st.spinner("Optimizing..."):
-        opt_var_c = 'c'
-        res_c = find_minimum(objective_function, initial_params_c, opt_var_c)
-        res_ir_c = find_minimum(objective_function_ir_ratio, initial_params_ir_c, opt_var_c)
-        res_ep_c = find_minimum(objective_function_ep_rate, initial_params_ep_c, opt_var_c)
-
-
-    df = pd.DataFrame({
-        'I': [np.NaN, init_I, np.NaN],
-        f's [e-{POWER_OF_10:.0f}]': [np.NaN, np.NaN, init_s],
-        'c*_g': [res_c.x[0], res_ir_c.x[0], res_ep_c.x[0]],
-        'c*_p': [res_c.x[1], res_ir_c.x[1], res_ep_c.x[1]],
-        f'min(w) [e-{POWER_OF_10:.0f}]': np.array([res_c.fun, res_ir_c.fun, res_ep_c.fun]) * MULTIPLIER
-        }, 
-        index=['reversibility', 'irreversibility ratio', 'entropy production rate']
-    )
-
-
-
-    # #=======PLOT=======
-    # # Generate data for surface plot
-    step_size = 0.005
-    c_g_values = np.arange(0.05, 0.50001, step_size)
-    c_p_values = np.arange(0.05, 0.50001, step_size)
-    X, Y = np.meshgrid(c_g_values, c_p_values)
-    Z = objective_function([X, Y], initial_params_c, opt_var_c) * MULTIPLIER
-    Z_ir = objective_function_ir_ratio([X, Y], initial_params_ir_c, opt_var_c) * MULTIPLIER
-    Z_ep = objective_function_ep_rate([X, Y], initial_params_ep_c, opt_var_c) * MULTIPLIER
-
-    # contraint line
-    if init_c_total <= 0.55: 
-        X_line = np.linspace(0.05, init_c_total - 0.05, 21)
-    else: X_line = np.linspace(init_c_total - 0.5, 0.5, 21)
-
-    min_line_lim = min(np.min(Z), np.min(Z_ir), np.min(Z_ep))
-    max_line_lim = max(np.max(Z), np.max(Z_ir), np.max(Z_ep))
-
-    X_line, Z_line = np.meshgrid(X_line, np.linspace(min_line_lim, max_line_lim, 21))
-    Y_line = init_c_total - X_line
-
-    # minimum points
-    x_r, y_r, z_r = res_c.x[0], res_c.x[1], res_c.fun * MULTIPLIER
-    x_ir, y_ir, z_ir = res_ir_c.x[0], res_ir_c.x[1], res_ir_c.fun * MULTIPLIER
-    x_ep, y_ep, z_ep = res_ep_c.x[0], res_ep_c.x[1], res_ep_c.fun * MULTIPLIER
-
-
-    contours = dict(
-        x=dict(
-            show=True,
-            usecolormap=True,
-            highlight=True,
-            highlightcolor="white",
-        ),
-        y=dict(
-            show=True,
-            usecolormap=True,
-            highlight=True,
-            highlightcolor="white",
-        )
-    )
-
-    fig = go.Figure()
-
-    fig.add_trace(go.Surface(
-        z=Z, x=X, y=Y, 
-        name='reversibility', legendgroup='reversibility', 
-        colorscale='Viridis', showlegend=True, showscale=False, opacity=0.75,
-        contours=contours
-    ))
-    fig.add_trace(go.Surface(
-        z=Z_ir, x=X, y=Y, 
-        name='irreversibility ratio', legendgroup='irreversibility',
-        colorscale='RdBu_r', showlegend=True, showscale=False, opacity=0.75,
-        contours=contours
-    ))
-    fig.add_trace(go.Surface(
-        z=Z_ep, x=X, y=Y, 
-        name='entropy production rate', legendgroup='entropy production', 
-        colorscale='rdylgn_r', showlegend=True, showscale=False, opacity=0.75,
-        contours=contours
-    ))
-    fig.add_trace(go.Surface(
-        z=Z_line, x=X_line, y=Y_line, 
-        name='constraint', legendgroup='constraint', 
-        colorscale=[[0, 'red'], [1, 'red']], showlegend=True, showscale=False, opacity=0.1,
-    ))
-    fig.add_trace(go.Scatter3d(
-        x=[x_r], y=[y_r], z=[z_r],
-        mode='markers',
-        name='minimum', legendgroup='minimum', showlegend=True,
-        marker=dict(size=5, color='red', symbol='circle'),
-    ))
-    fig.add_trace(go.Scatter3d(
-        x=[x_ir], y=[y_ir], z=[z_ir],
-        mode='markers',
-        name='minimum', legendgroup='minimum', showlegend=False,
-        marker=dict(size=5, color='red', symbol='circle'),
-    ))
-    fig.add_trace(go.Scatter3d(
-        x=[x_ep], y=[y_ep], z=[z_ep],
-        mode='markers',
-        name='minimum', legendgroup='minimum', showlegend=False,
-        marker=dict(size=5, color='red', symbol='circle'),
-    ))
-
-    fig.update_layout(
-        title=dict(text='3D Plot'), 
-        autosize=False,
-        width=600, height=500,
-        margin=dict(l=10, r=10, b=10, t=40),
-        scene=dict(
-            xaxis_title='<i>c<sub>g</sub></i>',
-            yaxis_title='<i>c<sub>p</sub></i>',
-            zaxis_title=f'<i>w · 10<sup>−{POWER_OF_10:.0f}</sup></i>',
-            xaxis_title_font=dict(family='STIX Two Math'),
-            yaxis_title_font=dict(family='STIX Two Math'),
-            zaxis_title_font=dict(family='STIX Two Math'),
-            aspectratio=dict(x=1, y=1, z=1),
-            camera=dict(
-                eye=dict(x=2, y=1, z=0.5)
-            )
-        ),
-        legend=dict(
-            yanchor="top",
-            y=1,
-            xanchor="center",
-            x=0.5,
-            orientation="h"
-        ),
-    )
+    initial_params = {
+        'r': [init_e_g, init_e_p, init_c_total, init_q, init_t_s, MULTIPLIER],
+        'ir': [init_e_g, init_e_p, init_c_total, init_q, init_t_s, init_I, MULTIPLIER],
+        'ep': [init_e_g, init_e_p, init_c_total, init_q, init_t_s, init_s, MULTIPLIER]
+    }
+    opt_var = 'c'
+    results = {
+        'r': find_minimum(objective_function, initial_params['r'], opt_var),
+        'ir': find_minimum(objective_function_ir_ratio, initial_params['ir'], opt_var),
+        'ep': find_minimum(objective_function_ep_rate, initial_params['ep'], opt_var)
+    }
 
 
     with col_plot:
-        st.plotly_chart(fig, use_container_width=True, config=config, key='plotly_c')
+        plotting3D(results, initial_params, opt_var)
 
     st.write('#### Results:')
+    df = pd.DataFrame({
+        'I': [np.NaN, init_I, np.NaN],
+        f's [e-{POWER_OF_10:.0f}]': [np.NaN, np.NaN, init_s],
+        'c*_g': [results['r'].x[0], results['ir'].x[0], results['ep'].x[0]],
+        'c*_p': [results['r'].x[1], results['ir'].x[1], results['ep'].x[1]],
+        f'min(w) [e-{POWER_OF_10:.0f}]': np.array([results['r'].fun, results['ir'].fun, results['ep'].fun]) * MULTIPLIER
+        }, 
+        index=['reversibility', 'irreversibility ratio', 'entropy production rate']
+    )
     st.dataframe(df)
+
 
 
 #=========SENSITIVITY ANALYSIS================
@@ -531,6 +292,15 @@ with tab_e:
 
     
     #========PLOT========
+    config = {
+        "toImageButtonOptions": {
+            "format": "png",  # The format of the exported image (png, svg, etc.)
+            "filename": "surface_plot",  # Default filename
+            # "height": 1080,  # Image height
+            # "width": 1920,   # Image width
+            "scale": 3       # Increase the resolution (scales up the image)
+        }
+    }
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(
